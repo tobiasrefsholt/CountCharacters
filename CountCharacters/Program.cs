d542bdfaaae338ac566fae349b7f6e28fcd32a69
tobias <tobias@refsholt.net>
2023-11-14 22:52:52 +0100

    Init

---
+++ CountCharacters/Program.cs
@@ -0,0 +1,21 @@
+﻿var range = 250;
+var counts = new int[range];
+string? text = "something";
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine();
+    if (text == null) continue;
+    foreach (var character in text.ToUpper() ?? string.Empty)
+    {
+        counts[(int)character]++;
+    }
+    for (var i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            int percentage = (int)Math.Round((double)counts[i] / text.Length * 100d);
+            Console.WriteLine($"{character} - {counts[i]} - {percentage}%" );
+        }
+    }
+}
